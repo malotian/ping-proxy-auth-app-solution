7442ff0f6654d7f699cae3f80b8fe149af6b47a5
Harjinder Dhanjal <malotian@gmail.com>
2025-04-20 15:18:19 -0400

    fixes

--- proxy/config.js
+++ proxy/config.js
@@ -5,7 +5,7 @@ module.exports = {
   mainAppUrl: process.env.MAIN_APP_URL || 'http://app.lab.com:3001',
   authServiceUrl: process.env.AUTH_SERVICE_URL || 'http://auth.lab.com:3002/advice',
   authServiceTarget: process.env.AUTH_SERVICE_TARGET || 'http://auth.lab.com:3002',
-  identityServiceUrl: process.env.AUTH_SERVICE_TARGET || 'https://openam-staplesciam-use4-dev.id.forgerock.io', 
+  identityServiceUrl: process.env.IDENTITY_SERVICE_TARGET || 'https://openam-staplesciam-use4-dev.id.forgerock.io', 
   tlsKey: process.env.TLS_KEY ||'/certs/key.pem',
   tlsCert: process.env.TLS_SECRET || '/certs/cert.pem'
 };
